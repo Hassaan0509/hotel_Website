cf677fd66f2fbb26bca54082692a87a42ad7d7c0
Hassaan Ahmad <2020CS678@student.uet.edu.pk>
2023-02-02 19:53:47 +0500

    State settled

--- src/State/store.jsx
+++ src/State/store.jsx
@@ -1,12 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { mode } from "./reducers/mode.jsx";
 import { locationreducer } from "./reducers/location.jsx";
+import { dateShow } from "./reducers/openDate.jsx";
+import { searchOption } from "./reducers/searchOption.jsx";
+import { dateReducer } from "./reducers/date.jsx";
+import { maxPrice } from "./reducers/maxPrice.jsx";
+import { minPrice } from "./reducers/minPrice.jsx";
 
 const store = configureStore({
   reducer: {
     theme: mode,
     location: locationreducer,
+    date: dateReducer,
+    max: maxPrice,
+    min: minPrice,
+    openDate: dateShow,
+    searchOption: searchOption,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
